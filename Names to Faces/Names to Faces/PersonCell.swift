682c5acc208d1f3fe798e544ba98fc1ac20e16af
RobertJG17 <robertjguerra@yahoo.com>
2020-10-19 12:27:42 -0700

    Completed Names to Faces Project

---
+++ Names to Faces/Names to Faces/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Names to Faces
+//
+//  Created by Robert Guerra on 10/18/20.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
---
+++ Names to Faces/Names to Faces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Names to Faces
+//
+//  Created by Robert Guerra on 10/18/20.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
