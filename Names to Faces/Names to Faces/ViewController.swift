2564206a824cb703312665f904a1c83aef9dd767
RobertJG17 <robertjguerra@yahoo.com>
2020-10-19 15:55:07 -0700

    Created Custom Cell and Implemented Collection View Methods to access said Custom Cell

--- Names to Faces/Names to Faces/ViewController.swift
+++ Names to Faces/Names to Faces/ViewController.swift
@@ -13,6 +13,23 @@ class ViewController: UICollectionViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            
+            // we failed to get a PersonCell - bail out!
+            
+            fatalError("Unable to dequeue reusable cell")
+        }
+        
+        // if we're stil here it means we got a PersonCell, so we can return it
+        
+        return cell
+    }
 
 
 }
