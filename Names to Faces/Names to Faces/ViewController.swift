44b38d55936166e66e77bc7baccdcf4974dd9511
RobertJG17 <robertjguerra@yahoo.com>
2020-10-20 13:15:02 -0700

    Conformed to UIImagerPickerControllerDelegate to Allow User Image Selection

--- Names to Faces/Names to Faces/ViewController.swift
+++ Names to Faces/Names to Faces/ViewController.swift
@@ -7,12 +7,47 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
-
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
     }
+    
+    // MARK: - Selectors
+    
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    // MARK: - UIImagePickerControllerDelegate Method Conformancies
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        dismiss(animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    // MARK: - UICollectionViewController Method Overrides
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
@@ -20,17 +55,13 @@ class ViewController: UICollectionViewController {
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
-            
             // we failed to get a PersonCell - bail out!
             
             fatalError("Unable to dequeue reusable cell")
         }
         
         // if we're stil here it means we got a PersonCell, so we can return it
-        
         return cell
     }
-
-
 }
 
